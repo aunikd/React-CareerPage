dbfd0175e3cad728448c00f38491796934c27e09
Aunik <44595471+aunikd@users.noreply.github.com>
2025-04-21 02:11:00 +0530

    Major Push of all files
    Inlcudes jsx files and structure as well as JSON. Built using Vite

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+  Route, createBrowserRouter,
+  createRoutesFromElements, RouterProvider
+} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
+import JobPage, { jobLoader } from './pages/JobPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+
+const router = createBrowserRouter(
+
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' index element={<JobsPage />} />
+      <Route  path='/jobs/:id' index element={<JobPage />} loader={jobLoader}/>
+      <Route path='*' index element={<NotFoundPage />} />
+
+
+    </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />
+}
+
+export default App
